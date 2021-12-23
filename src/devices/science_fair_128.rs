5c3c7cc42147d29ac8fbebd89a4764b7916e2c82
Pavel Kasila <pavel.kasila@gmail.com>
2021-12-23 13:05:41 +0300

    Update syntax

--- src/devices/science_fair_128.rs
+++ src/devices/science_fair_128.rs
@@ -1,28 +1,26 @@
 use std::fs;
 use crate::devices::device::Device;
 
-const MAX_ALLOWED_FRAMERATE: i32 = 180;
-
 pub struct ScienceFair128 {}
 
 impl Device for ScienceFair128 {
     fn product_id(&self) -> String {
-        return "ScienceFair/0.1".to_string();
+        "ScienceFair/0.1".to_string()
     }
 
     fn serial_number(&self) -> String {
-        return fs::read_to_string("/boot/corevox/serial_number").unwrap_or("UNREGISTERED".to_string());
+        fs::read_to_string("/boot/corevox/serial_number").unwrap_or("UNREGISTERED".to_string())
     }
 
     fn max_framerate(&self) -> i32 {
-        return MAX_ALLOWED_FRAMERATE;
+        180
     }
 
     fn pov_frequency(&self) -> i32 {
-        return 24;
+        24
     }
 
     fn frame_size(&self) -> [i32; 2] {
-        return [128, 128];
+        [128, 128]
     }
 }
