f28a347f801c6690faf32f898a49e204ca6f40b4
Pavel Kasila <pavel.kasila@gmail.com>
2021-12-24 10:32:48 +0300

    add ScienceFair/0.2 (320x240) descriptor

---
+++ src/devices/science_fair_320.rs
@@ -0,0 +1,26 @@
+use std::fs;
+use crate::devices::device::Device;
+
+pub struct ScienceFair320 {}
+
+impl Device for ScienceFair320 {
+    fn product_id(&self) -> String {
+        "ScienceFair/0.2".to_string()
+    }
+
+    fn serial_number(&self) -> String {
+        fs::read_to_string("/boot/corevox/serial_number").unwrap_or("UNREGISTERED".to_string())
+    }
+
+    fn max_framerate(&self) -> i32 {
+        312
+    }
+
+    fn pov_frequency(&self) -> i32 {
+        24
+    }
+
+    fn frame_size(&self) -> [i32; 2] {
+        [320, 240]
+    }
+}
